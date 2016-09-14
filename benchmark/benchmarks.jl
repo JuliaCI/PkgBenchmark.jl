1bc964e1f1a9d167e0bfeefdf838b042703c84c7
Shashi Gowda <shashigowda91@gmail.com>
2016-09-14 13:45:44 +0530

    add a benchmark script

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,26 @@
+using BenchmarkHelper
+
+@benchgroup "utf8" ["string", "unicode"] begin
+    teststr = UTF8String(join(rand(MersenneTwister(1), 'a':'d', 10^4)))
+    @bench "replace" replace($teststr, "a", "b")
+    @bench "join" join($teststr, $teststr)
+end
+
+@benchgroup "trigonometry" ["math", "triangles"] begin
+    # nested groups
+    @benchgroup "circular" begin
+        for f in (sin, cos, tan)
+            for x in (0.0, pi)
+                @bench string(f) x $(f)($x)
+            end
+        end
+    end
+
+    @benchgroup "hyperbolic" begin
+        for f in (sinh, cosh, tanh)
+            for x in (0.0, pi)
+                @bench string(f) x $(f)($x)
+            end
+        end
+    end
+end
