46286e3f1e57e51611421368173ce434e66fa4c0
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-07-19 12:43:42 +0200

    inline the doc strings into the source code (#37)

--- src/judge.jl
+++ src/judge.jl
@@ -24,6 +24,31 @@ function withresults(f::Function, pkg::String, refs;
         for (r,s) in zip(refs, use_saved)] |> f
 end
 
+"""
+    judge(pkg, [ref], baseline;
+        f=(minimum, minimum),
+        usesaved=(true, true),
+        script=defaultscript(pkg),
+        require=defaultrequire(pkg),
+        resultsdir=defaultresultsdir(pkg),
+        saveresults=true,
+        promptsave=true,
+        promptoverwrite=true)
+
+You can call `showall(results)` to see a comparison of all the benchmarks.
+
+**Arguments**:
+
+- `pkg` is the package to benchmark
+- `ref` optional, the commit to judge. If skipped, use the current state of the package repo.
+- `baseline` is the commit to compare `ref` against.
+
+**Keyword arguments**:
+
+- `f` - tuple of estimator functions - one each for `from_ref`, `to_ref` respectively
+- `use_saved` - similar tuple of flags, if false will not use saved results
+- for description of other keyword arguments, see `benchmarkpkg`
+"""
 function BenchmarkTools.judge(pkg::String, ref1::Union{String,Void}, ref2::String; f=minimum, judgekwargs=Dict(), kwargs...)
     fs = _repeat(f, 2)
     withresults(rs->judge(map((f,x)->f(x), fs, rs)...; judgekwargs...), pkg, (ref1, ref2); kwargs...)
