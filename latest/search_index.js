341575de4dc2d06497c0bea10e9e3d337b543d9d
autodocs <autodocs>
2018-03-13 10:50:33 +0000

    build based on 6f16bfb

--- latest/search_index.js
+++ latest/search_index.js
@@ -60,7 +60,7 @@ var documenterSearchIndex = {"docs": [
     "location": "run_benchmarks.html#PkgBenchmark.benchmarkpkg",
     "page": "Running a benchmark suite",
     "title": "PkgBenchmark.benchmarkpkg",
-    "category": "Function",
+    "category": "function",
     "text": "benchmarkpkg(pkg, [target]::Union{String, BenchmarkConfig}; kwargs...)\n\nRun a benchmark on the package pkg using the BenchmarkConfig or git identifier target. Examples of git identifiers are commit shas, branch names, or e.g. \"HEAD~1\". Return a BenchmarkResults.\n\nThe argument pkg can be a name of a package or a path to a directory to a package.\n\nKeyword arguments:\n\nscript - The script with the benchmarks, if not given, defaults to benchmark/benchmarks.jl in the package folder.\nresultfile - If set, saves the output to resultfile\nretune - Force a re-tune, saving the new tuning to the tune file.\n\nThe result can be used by functions such as judge. If you choose to, you can save the results manually using writeresults where results is the return value of this function. It can be read back with readresults.\n\nIf a REQUIRE file exists in the same folder as script, load package requirements from that file before benchmarking.\n\nExample invocations:\n\nusing PkgBenchmark\n\nbenchmarkpkg(\"MyPkg\") # run the benchmarks at the current state of the repository\nbenchmarkpkg(\"MyPkg\", \"my-feature\") # run the benchmarks for a particular branch/commit/tag\nbenchmarkpkg(\"MyPkg\", \"my-feature\"; script=\"/home/me/mycustombenchmark.jl\")\nbenchmarkpkg(\"MyPkg\", BenchmarkConfig(id = \"my-feature\",\n                                      env = Dict(\"JULIA_NUM_THREADS\" => 4),\n                                      juliacmd = `julia -O3`))\n\n\n\n"
 },
 
@@ -68,7 +68,7 @@ var documenterSearchIndex = {"docs": [
     "location": "run_benchmarks.html#PkgBenchmark.BenchmarkResults",
     "page": "Running a benchmark suite",
     "title": "PkgBenchmark.BenchmarkResults",
-    "category": "Type",
+    "category": "type",
     "text": "Stores the results from running the benchmarks on a package.\n\nThe following (unexported) methods are defined on a BenchmarkResults (written below as results):\n\nname(results)::String - The commit of the package benchmarked\ncommit(results)::String - The commit of the package benchmarked. If the package repository was dirty, the string \"dirty\" is returned.\njuliacommit(results)::String - The commit of the Julia executable that ran the benchmarks\nbenchmarkgroup(results)::BenchmarkGroup - a BenchmarkGroup  contaning the results of the benchmark.\ndate(results)::DateTime - Tthe time when the benchmarks were executed\nbenchmarkconfig(results)::BenchmarkConfig - The BenchmarkConfig used for the benchmarks.\n\nBenchmarkResults can be exported to markdown using the function export_markdown.\n\n\n\n"
 },
 
@@ -100,7 +100,7 @@ var documenterSearchIndex = {"docs": [
     "location": "comparing_commits.html#BenchmarkTools.judge",
     "page": "Comparing commits",
     "title": "BenchmarkTools.judge",
-    "category": "Function",
+    "category": "function",
     "text": "judge(pkg::String,\n      [target]::Union{String, BenchmarkConfig},\n      baseline::Union{String, BenchmarkConfig};\n      kwargs...)\n\nArguments:\n\npkg - The package to benchmark.\ntarget - What do judge, given as a git id or a BenchmarkConfig. If skipped, use the current state of the package repo.\nbaseline - The commit / BenchmarkConfig to compare target against.\n\nKeyword arguments:\n\nf - Estimator function to use in the judging.\njudgekwargs::Dict{Symbol, Any} - keyword arguments to pass to the judge function in BenchmarkTools\n\nThe remaining keyword arguments are passed to benchmarkpkg\n\nReturn value:\n\nReturns a BenchmarkJudgement\n\n\n\njudge(target::BenchmarkResults, baseline::BenchmarkResults, f;\n      judgekwargs = Dict())\n\nJudges the two BenchmarkResults in target and baseline using the function f.\n\nReturn value\n\nReturns a BenchmarkJudgement\n\n\n\n"
 },
 
@@ -124,7 +124,7 @@ var documenterSearchIndex = {"docs": [
     "location": "export_markdown.html#PkgBenchmark.export_markdown",
     "page": "Export to markdown",
     "title": "PkgBenchmark.export_markdown",
-    "category": "Function",
+    "category": "function",
     "text": "export_markdown(file::String, results::Union{BenchmarkResults, BenchmarkJudgement})\nexport_markdown(io::IO,       results::Union{BenchmarkResults, BenchmarkJudgement})\n\nWrites the results to file or io in markdown format.\n\nSee also: BenchmarkResults, BenchmarkJudgement\n\n\n\n"
 },
 
@@ -148,7 +148,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.BenchmarkConfig",
     "page": "Reference",
     "title": "PkgBenchmark.BenchmarkConfig",
-    "category": "Type",
+    "category": "type",
     "text": "BenchmarkConfig\n\nA BenchmarkConfig contains the configuration for the benchmarks to be executed by benchmarkpkg.\n\nThis includes the following:\n\nThe commit of the package the benchmarks are run on.\nWhat julia command should be run, i.e. the path to the Julia executable and the command flags used (e.g. optimization level with -O).\nCustom environment variables (e.g. JULIA_NUM_THREADS).\n\n\n\n"
 },
 
@@ -156,7 +156,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.BenchmarkConfig-Tuple{}",
     "page": "Reference",
     "title": "PkgBenchmark.BenchmarkConfig",
-    "category": "Method",
+    "category": "method",
     "text": "BenchmarkConfig(;id::Union{String, Void} = nothing,\n                 juliacmd::Cmd = `joinpath(JULIA_HOME, Base.julia_exename())`,\n                 env::Dict{String, Any} = Dict{String, Any}())\n\nCreates a BenchmarkConfig from the following keyword arguments:\n\nid - A git identifier like a commit, branch, tag, \"HEAD\", \"HEAD~1\" etc.        If id == nothing then benchmark will be done on the current state        of the repo (even if it is dirty).\njuliacmd - Used to exectue the benchmarks, defaults to the julia executable              that the Pkgbenchmark-functions are called from. Can also include command flags.\nenv - Contains custom environment variables that will be active when the         benchmarks are run.\n\nExamples\n\njulia> using Pkgbenchmark\n\njulia> BenchmarkConfig(id = \"performance_improvements\",\n                       juliacmd = `julia -O3`,\n                       env = Dict(\"JULIA_NUM_THREADS\" => 4))\nBenchmarkConfig:\n    id: performance_improvements\n    juliacmd: `julia -O3`\n    env: JULIA_NUM_THREADS => 4\n\n\n\n"
 },
 
@@ -164,7 +164,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.BenchmarkJudgement",
     "page": "Reference",
     "title": "PkgBenchmark.BenchmarkJudgement",
-    "category": "Type",
+    "category": "type",
     "text": "Stores the results from running a judgement, see judge.\n\nThe following (unexported) methods are defined on a BenchmarkJudgement (written below as judgement):\n\ntarget_result(judgement)::BenchmarkResults - the BenchmarkResults of the target.\nbaseline_result(judgement)::BenchmarkResults -  the BenchmarkResults of the baseline.\nbenchmarkgroup(judgement)::BenchmarkGroup - a BenchmarkGroup  contaning the estimated results\n\nA BenchmarkJudgement can be exported to markdown using the function export_markdown.\n\nSee also BenchmarkResults\n\n\n\n"
 },
 
@@ -172,7 +172,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#BenchmarkTools.judge",
     "page": "Reference",
     "title": "BenchmarkTools.judge",
-    "category": "Function",
+    "category": "function",
     "text": "judge(target::BenchmarkResults, baseline::BenchmarkResults, f;\n      judgekwargs = Dict())\n\nJudges the two BenchmarkResults in target and baseline using the function f.\n\nReturn value\n\nReturns a BenchmarkJudgement\n\n\n\n"
 },
 
@@ -180,7 +180,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#BenchmarkTools.judge-Tuple{String,Union{PkgBenchmark.BenchmarkConfig, String},Union{PkgBenchmark.BenchmarkConfig, String}}",
     "page": "Reference",
     "title": "BenchmarkTools.judge",
-    "category": "Method",
+    "category": "method",
     "text": "judge(pkg::String,\n      [target]::Union{String, BenchmarkConfig},\n      baseline::Union{String, BenchmarkConfig};\n      kwargs...)\n\nArguments:\n\npkg - The package to benchmark.\ntarget - What do judge, given as a git id or a BenchmarkConfig. If skipped, use the current state of the package repo.\nbaseline - The commit / BenchmarkConfig to compare target against.\n\nKeyword arguments:\n\nf - Estimator function to use in the judging.\njudgekwargs::Dict{Symbol, Any} - keyword arguments to pass to the judge function in BenchmarkTools\n\nThe remaining keyword arguments are passed to benchmarkpkg\n\nReturn value:\n\nReturns a BenchmarkJudgement\n\n\n\n"
 },
 
@@ -188,7 +188,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.benchmarkpkg",
     "page": "Reference",
     "title": "PkgBenchmark.benchmarkpkg",
-    "category": "Function",
+    "category": "function",
     "text": "benchmarkpkg(pkg, [target]::Union{String, BenchmarkConfig}; kwargs...)\n\nRun a benchmark on the package pkg using the BenchmarkConfig or git identifier target. Examples of git identifiers are commit shas, branch names, or e.g. \"HEAD~1\". Return a BenchmarkResults.\n\nThe argument pkg can be a name of a package or a path to a directory to a package.\n\nKeyword arguments:\n\nscript - The script with the benchmarks, if not given, defaults to benchmark/benchmarks.jl in the package folder.\nresultfile - If set, saves the output to resultfile\nretune - Force a re-tune, saving the new tuning to the tune file.\n\nThe result can be used by functions such as judge. If you choose to, you can save the results manually using writeresults where results is the return value of this function. It can be read back with readresults.\n\nIf a REQUIRE file exists in the same folder as script, load package requirements from that file before benchmarking.\n\nExample invocations:\n\nusing PkgBenchmark\n\nbenchmarkpkg(\"MyPkg\") # run the benchmarks at the current state of the repository\nbenchmarkpkg(\"MyPkg\", \"my-feature\") # run the benchmarks for a particular branch/commit/tag\nbenchmarkpkg(\"MyPkg\", \"my-feature\"; script=\"/home/me/mycustombenchmark.jl\")\nbenchmarkpkg(\"MyPkg\", BenchmarkConfig(id = \"my-feature\",\n                                      env = Dict(\"JULIA_NUM_THREADS\" => 4),\n                                      juliacmd = `julia -O3`))\n\n\n\n"
 },
 
@@ -196,7 +196,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.export_markdown-Tuple{String,PkgBenchmark.BenchmarkResults}",
     "page": "Reference",
     "title": "PkgBenchmark.export_markdown",
-    "category": "Method",
+    "category": "method",
     "text": "export_markdown(file::String, results::Union{BenchmarkResults, BenchmarkJudgement})\nexport_markdown(io::IO,       results::Union{BenchmarkResults, BenchmarkJudgement})\n\nWrites the results to file or io in markdown format.\n\nSee also: BenchmarkResults, BenchmarkJudgement\n\n\n\n"
 },
 
@@ -204,7 +204,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.readresults-Tuple{String}",
     "page": "Reference",
     "title": "PkgBenchmark.readresults",
-    "category": "Method",
+    "category": "method",
     "text": "readresults(file::String)\n\nReads the BenchmarkResults stored in file (given as a path).\n\n\n\n"
 },
 
@@ -212,7 +212,7 @@ var documenterSearchIndex = {"docs": [
     "location": "ref.html#PkgBenchmark.writeresults-Tuple{String,PkgBenchmark.BenchmarkResults}",
     "page": "Reference",
     "title": "PkgBenchmark.writeresults",
-    "category": "Method",
+    "category": "method",
     "text": "writeresults(file::String, results::BenchmarkResults)\n\nWrites the BenchmarkResults to file.\n\n\n\n"
 },
 
