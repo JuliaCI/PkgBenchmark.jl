7c771b2f50b9fb16e7606ee5dfa12450fb5b10a7
one-more-fix <62365640+one-more-fix@users.noreply.github.com>
2020-03-18 22:40:34 -0400

    More spellchecks

--- src/benchmarkjudgement.jl
+++ src/benchmarkjudgement.jl
@@ -6,7 +6,7 @@ The following (unexported) methods are defined on a `BenchmarkJudgement` (writte
 * `target_result(judgement)::BenchmarkResults` - the [`BenchmarkResults`](@ref) of the `target`.
 * `baseline_result(judgement)::BenchmarkResults` -  the [`BenchmarkResults`](@ref) of the `baseline`.
 * `benchmarkgroup(judgement)::BenchmarkGroup` - a [`BenchmarkGroup`](https://github.com/JuliaCI/BenchmarkTools.jl/blob/master/doc/manual.md#the-benchmarkgroup-type)
-   contaning the estimated results
+   containing the estimated results
 
 A `BenchmarkJudgement` can be exported to markdown using the function [`export_markdown`](@ref).
 
