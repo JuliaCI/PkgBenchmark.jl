df540524a1ad3016e7d13cfb7cdffd65b1b096a4
Kristoffer Carlsson <kcarlsson89@gmail.com>
2021-02-01 17:12:07 +0100

    delete "deprecation"

--- deps/build.jl
+++
@@ -1,9 +0,0 @@
-if !isfile(joinpath(@__DIR__, "already_showed"))
-    printstyled(stderr,
-    """
-    PkgBenchmark has been completely rewritten. Please see https://github.com/JuliaCI/PkgBenchmark.jl/
-    for updated documentation and examples. Code written for previous versions of PkgBenchmark is
-    unlikely to still work.
-    """; color = Base.info_color())
-    touch("already_showed")
-end
